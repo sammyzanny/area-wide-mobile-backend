8ac1d45ba8af0228997b09cf0384e4c9a5ac4660
github username <github email address>
2020-10-20 21:21:08 -0500

    initial backend structure

---
+++ app/controllers/user_controller.rb
@@ -0,0 +1,50 @@
+class UsersController < ApplicationController
+  skip_before_action :authorized, only: [:create, :index, :visit, :item_search, :name_search]
+
+  def profile
+    render json: { user: UserSerializer.new(current_user) }, status: :accepted
+  end
+
+  def update
+    user = current_user
+    user.update_attributes(user_params)
+    if user.valid?
+      render json: user
+    else 
+      render json: {error: "Profile update did not go through"}
+    end
+  
+  end
+
+  def visit
+    user = User.find_by(username: params[:username])
+    if user 
+      render json: { user: UserSerializer.new(user) }, status: :accepted
+    else
+      render json: { error: 'User not found' }, status: :not_acceptable
+    end
+
+  end
+
+  def index
+    users = User.all
+    render json: users
+  end
+
+  def create
+    @user = User.create(user_params)
+    if @user.valid?
+      @token = encode_token(user_id: @user.id)
+      render json: { user: UserSerializer.new(@user), jwt: @token }, status: :created
+    else
+      render json: { error: 'failed to create user' }, status: :not_acceptable
+    end
+  end
+
+  private
+
+  def user_params
+    params.require(:user).permit(:username, :password, :name, :city, :phone, :email)
+  end
+
+end
