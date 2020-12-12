aef37774d7d5d81afc6ab1d9694a743e8b6693bf
github username <github email address>
2020-12-12 16:59:07 -0600

    hide email

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -89,8 +89,8 @@ Rails.application.configure do
   config.action_mailer.smtp_settings = {
   :address              => "smtp.gmail.com",
   :port                 => 587,
-  :user_name            => "areawidesender@gmail.com",
-  :password             => "Areawide_3",
+  :user_name            => ENV['EMAIL_SENDER'],
+  :password             => ENV['EMAIL_PASSWORD'],
   :authentication       => "plain",
   :enable_starttls_auto => true
   }
