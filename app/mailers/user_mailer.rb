c546679a4d091df029db5bf398b2383d3153e36e
github username <github email address>
2021-02-19 15:12:28 -0600

    Email Change

--- app/mailers/user_mailer.rb
+++ app/mailers/user_mailer.rb
@@ -7,7 +7,9 @@ class UserMailer < ApplicationMailer
         @post.images.each_with_index do |file, q|
             attachments[file.blob.filename.to_s] = { mime_type: file.blob.content_type, content: file.blob.download }
           end
+        mail(to: 'awwpinc@gmail.com', subject: "Crack Upload from #{@user.name}")
         mail(to: 'muffinmannnnnn@gmail.com', subject: "Crack Upload from #{@user.name}")
+
     end
 
     def reset_email
