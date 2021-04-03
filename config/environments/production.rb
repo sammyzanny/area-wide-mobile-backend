82f2aeab5b363d44825714db9d1ac0716550dd0a
github username <github email address>
2021-04-03 15:46:22 -0500

    mailgun

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -84,7 +84,7 @@ Rails.application.configure do
   # Do not dump schema after migrations.
   config.active_record.dump_schema_after_migration = false
 
-  ActionMailer::Base.delivery_method = :smtp
+  config.action_mailer.delivery_method = :smtp
   # SMTP settings for gmail
   # config.action_mailer.smtp_settings = {
   # :address              => "smtp.gmail.com",
@@ -95,7 +95,7 @@ Rails.application.configure do
   # :enable_starttls_auto => true
   # }
 
-  ActionMailer::Base.smtp_settings = {
+  config.action_mailer.smtp_settings = {
   :port           => 587,
   :address        => ENV['MAILGUN_SMTP_SERVER'],
   :user_name      => ENV['MAILGUN_SMTP_LOGIN'],
