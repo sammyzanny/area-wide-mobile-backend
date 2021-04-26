91f065ae70f270ae76124d7cd5a20628ac2948e2
github username <github email address>
2021-04-25 21:31:42 -0500

    both emails added

--- app/mailers/user_mailer.rb
+++ app/mailers/user_mailer.rb
@@ -7,7 +7,7 @@ class UserMailer < ApplicationMailer
         @post.images.each_with_index do |file, q|
             attachments[file.blob.filename.to_s] = { mime_type: file.blob.content_type, content: file.blob.download }
           end
-        mail(to: 'muffinmannnnnn@gmail.com, samuelczandi@gmail.com', subject: "Crack Upload from #{@user.name}")
+        mail(to: 'muffinmannnnnn@gmail.com, awwpinc@gmail.com', subject: "Crack Upload from #{@user.name}")
 
     end
 
